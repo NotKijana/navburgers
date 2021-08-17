5926c51cceb323b1b38d934d992c36c2e78af3ac
Not Kijana <Cloudhashira@icloud.com>
2021-08-17 00:32:42 -0400

    Changed transition to top 2nd Button

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const disappearBurger = document.querySelector('#disappear');
+const collapseBurger = document.querySelector('#collapse')
 
 disappearBurger.addEventListener('click', function() {
     disappearBurger.classList.toggle('open')
+})
+
+collapseBurger.addEventListener('click', function() {
+    collapseBurger.classList.toggle('open')
 })
