ee041f9156a2713581b1de8ff36e34f6e5ff5380
Not Kijana <cloudhashira@icloud.com>
2021-08-17 23:37:08 -0400

    Finished last 3 added styles

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const disappearBurger = document.querySelector('#disappear');
-const collapseBurger = document.querySelector('#collapse')
+const collapseBurger = document.querySelector('#collapse');
+const spinBurger = document.querySelector('#spin');
+const standBurger = document.querySelector('#stand');
+const minusBurger = document.querySelector('#minus');
+const basicBurger = document.querySelector('#basic');
 
 disappearBurger.addEventListener('click', function() {
     disappearBurger.classList.toggle('open')
@@ -7,4 +11,20 @@ disappearBurger.addEventListener('click', function() {
 
 collapseBurger.addEventListener('click', function() {
     collapseBurger.classList.toggle('open')
+})
+
+spinBurger.addEventListener('click', function() {
+    spinBurger.classList.toggle('open')
+})
+
+standBurger.addEventListener('click', function() {
+    standBurger.classList.toggle('open')
+})
+
+minusBurger.addEventListener('click', function() {
+    minusBurger.classList.toggle('open')
+})
+
+basicBurger.addEventListener('click', function() {
+    basicBurger.classList.toggle('open')
 })
